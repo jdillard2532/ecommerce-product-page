c1aa81d1d2d6cd5b76eff1141824c0e1d164e583
jdillard2532 <125132247+jdillard2532@users.noreply.github.com>
2025-01-21 13:52:39 -0500

    Navbar Update
    Updated NavBar

--- app.js
+++ app.js
@@ -1,40 +1,58 @@
 const menuImage = document.querySelector(".menu");
-const navbar = document.querySelector(".navbar");
 const navbarNav = document.querySelectorAll(".navbar-nav");
-const navMenuDiv = document.querySelector(".navbar div");
+const mainNavbar = navbarNav[0];
 const listItem = document.querySelectorAll(".list-item");
-const body = document.querySelector("body");
 const overlay = document.querySelector(".overlay");
 const logo = document.querySelector(".logo");
 
 menuImage.addEventListener("click", () => {
-  let mainNavbar = navbarNav[0];
+  let mainList = document.querySelectorAll(".list-item a");
+  let listItemMain = document.querySelectorAll(".list-item-main");
+  console.log(listItemMain);
+  console.log(mainList);
   if (mainNavbar.style.display === "" || mainNavbar.style.display === "none") {
     mainNavbar.style.display = "flex";
     menuImage.src = "./images/icon-close.svg";
     mainNavbar.classList.add("activeMenu");
     overlay.style.display = "block";
-    // logo.style.display = "none";
-    for (let i = 0; i < listItem.length; i++) {
-      listItem[i].style.paddingBottom = "1rem";
-      listItem[i].style.maxWidth = "10%";
-    }
+    logo.style.display = "none";
+    mainList.forEach((e) => {
+      e.style.paddingBottom = "1rem";
+    });
+    // listItemMain.forEach((e) => {
+    //   e.style.paddingBottom = "0";
+    // });
   } else {
     menuImage.src = "./images/icon-menu.svg";
     mainNavbar.style.display = "none";
     overlay.style.display = "none";
-    // logo.style.display = "block";
+    logo.style.display = "block";
+    mainNavbar.classList.remove("activeMenu");
+    // mainList.forEach((e) => {
+    //   e.style.paddingBottom = "2.81rem";
+    // });
+    // listItemMain.forEach((e) => {
+    //   e.style.paddingBottom = "2.81rem";
+    // });
   }
 });
 
-// function windowSize(){
-//   let windowWidth =
-// }
-
 addEventListener("resize", () => {
   let windowWidth = window.innerWidth;
-  if (windowWidth > 768) {
-    navbarNav.classList.remove("activeMenu");
+  if (windowWidth > 800) {
+    mainNavbar.classList.remove("activeMenu");
     overlay.style.display = "none";
+    logo.style.display = "block";
+    mainNavbar.style.display = "flex";
+    navbarNav.forEach((e) => {
+      navbarNav.style.display = "flex";
+    });
+  }
+  if (windowWidth <= 799) {
+    mainNavbar.classList.remove("activeMenu");
+    mainNavbar.style.display = "none";
+    navbarNav.forEach((e) => {
+      navbarNav.style.display = "none";
+    });
   }
 });
